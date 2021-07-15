fed84160266e613cf76f875f63817004b8de962d
Florian Lagg <florian.lagg@gmail.com>
2021-07-16 00:44:21 +0200

    pushed govee integration - daily json scraper

---
+++ goveestats_installations.py
@@ -0,0 +1,16 @@
+import scrapy
+import json
+import datetime
+
+class GoveestatsInstallationsSpider(scrapy.Spider):
+    name = 'goveestats-installations'
+    #allowed_domains = ['analytics.home-assistant.io']
+    start_urls = ['https://analytics.home-assistant.io/custom_integrations.json']
+
+    def parse(self, response):
+
+        jsonresponse = json.loads(response.body_as_unicode())
+        return {
+            'timestamp': datetime.datetime.now(datetime.timezone.utc),
+            'installations': jsonresponse['govee']
+        }
